46499cf73f8a33e7810af910e334f662b75f4e2e
Ihor Zinchenko <igor.zinchenko@unibrix.com>
2025-05-12 12:04:32 +0300

    fix: select the correct item on keydown in the multiple mode

--- src/use-autocomplete.ts
+++ src/use-autocomplete.ts
@@ -132,7 +132,7 @@ export function useAutoComplete(
     defaultValue: defaultValues.map(v => v?.toString()),
     value: valuesProp,
     onChange: (newValues: any[]) => {
-      const item = filteredList.find(opt => opt.value === newValues[0]);
+      const item = filteredList.find(opt => opt.value === newValues.at(-1));
       if (!item) return;
       const items = newValues.map(val =>
         filteredList.find(opt => opt.value === val)
@@ -242,7 +242,7 @@ export function useAutoComplete(
       if(!item && creatable === true) {
         item = {label: itemValue, value: itemValue};
       }
-      
+
       if (!item) {
         return prevValues;
       }
